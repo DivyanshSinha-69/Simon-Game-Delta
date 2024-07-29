4650edb1030f52cf466c4a8e474a6a46bf1158bd
Divyansh Sinha <harshsinha190@gmail.com>
2024-07-30 03:46:19 +0530

    Added main functionality of game

--- app.js
+++ app.js
@@ -33,19 +33,53 @@ function userFlash(btn) {
 }
 
 function levelUp() {
+    userSeq = [];
     level++;
     h2.innerText = `Level ${level}`;
 
     let randIdx = Math.floor(Math.random()*4);
     let randColor = btns[randIdx];
     let randBtn = document.querySelector(`.${randColor}`);
-
+    
+    gameSeq.push(randColor);
     gameFlash(randBtn);
+}
+
+function checkAns() {
+    let idx = level-1;
+
+    if(userSeq[idx] === gameSeq[idx])
+    {
+        if(userSeq.length==gameSeq.length)
+        {
+            setTimeout(function() {
+                levelUp();
+            }, 1000);
+        }
+    }
+    else
+    {
+        h2.innerText = `Game Over! Press any key to start.`;
+        let body = document.querySelector("body");
+        body.classList.add("Game-Over");
+        setTimeout(function() {
+            body.classList.remove("Game-Over")
+        }, 1000);
+        
+        level=0;
+        gameSeq = [];
+        userSeq = [];
+        started = false;
+    }
 }
 
 function btnPress() {
     let btn = this;
-    userFlash(btn);    
+    userSeq.push(this.id);
+    console.log(userSeq);
+    userFlash(btn);
+
+    checkAns();
 }
 
 let allBtns = document.querySelectorAll(".btn");
